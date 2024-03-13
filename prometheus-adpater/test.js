660e742b12f6deb882de41bee7b30f51f731107d
sooeon <miantndjs@naver.com>
2024-03-13 15:00:03 +0900

    complete istio-hpa

--- prometheus-adpater/test.js
+++ prometheus-adpater/test.js
@@ -2,29 +2,72 @@ import http from 'k6/http';
 import { check, sleep } from 'k6';
 
 export const options = {
-    stages: [
-        { duration: '1m', target: 3 },
-        { duration: '1m', target: 5 },
-        { duration: '3m', target: 5 },
-        { duration: '1m', target: 3 },
-        { duration: '2m', target: 3 },
-        { duration: '2m', target: 3 },
-    ]
-}
+  stages: [
+    { duration: '60s', target: 10 },
+    { duration: '60s', target: 1 },
+  ]
+};
 
 export default function () {
-  const url = 'http://10.103.202.249/apis/v1/product/1';
-  const response = http.get(url);
-  check(response, {
-    'status was 200': (r) => r.status == 200,
-  })
 
-  // check res
-  if (response.status >= 500) {
-    console.log('서버 오류 발생!');
-    console.log('상태 코드:', response.status);
-  } else {
-    console.log('요청이 성공적으로 처리되었습니다.', response.status);
-  }
+  // //user, rating test
+
+  // console.log("--------------------------------------------------------");
+  // console.log("user create and delete playbook");
+  // console.log("create user");
+  
+  // const username = Math.random().toString(36).substring(2, 10);
+  // const data = {
+  //   username: username,
+  //   password: "1234",
+  //   email: "abcd@abcd.com",
+  //   phone_number: "01011111111"
+  // };
+  
+  // const createUserUrl = "http://10.100.0.113/apis/v1/user";
+  // const createUserHeaders = { 'Content-Type': 'application/json' };
+  // const createUserResponse = http.post(createUserUrl, JSON.stringify(data), { headers: createUserHeaders });
+  // const user_id = createUserResponse.json().payload.user_id;
+  
+  // if (!username) {
+  //   console.log("Error: username doesn't exist");
+  //   return;
+  // }
+  
+  // http.get("http://10.100.0.113/apis/v1/ratings/" + user_id);
+  
+  // console.log("\ndelete user");
+  
+  // const deleteUserUrl = "http://10.100.0.113/apis/v1/user/" + user_id;
+  // const deleteUserHeaders = { 'Content-Type': 'application/json' };
+  // http.del(deleteUserUrl, null, { headers: deleteUserHeaders });
+  
+  // sleep(3);
+
+  
+
+  ///////////////////////////////////////////////////////////////////////////////////////
+
+  // load test + hpa test + canary test 
+  console.log("load test + product circuit breaking test + canry test")
+
+  let urls = [
+    "http://10.100.0.113/apis/v1/product/1", //상품목록
+    "http://10.100.0.113/apis/v1/user/1", // user
+    "http://10.100.0.113/apis/v1/ratings/1", // rating (매너온도)
+    "http://10.100.0.113/apis/v1/carts/users/1", // 찜목록
+    "http://10.100.0.113/apis/v1/order/1", // 주문목록
+    "http://10.100.0.113/apis/v1/order/sale/1", // 판매목록
+    "http://10.100.0.113/apis/v1/order/purchase/1", // 구매목록
+  ];
+  let randomIndex = Math.floor(Math.random() * urls.length);
+  let url = urls[randomIndex];
+  http.get(url);
+
   sleep(1);
+
+
+  // product circuit breaking test + hpa test
+
+
 }
